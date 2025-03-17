d814de69cfa5777c0facb33d1958236dcc83bd95
rrasp94 <ruraspud@vsite.hr>
2025-03-17 16:11:32 +0100

    v01 solved

--- main.cpp
+++ main.cpp
@@ -4,12 +4,24 @@ enum { id_button1 = 1, id_button2 };
 
 void on_create(HWND hw) 
 {
-	// TODO: create two child windows of type button
+	::CreateWindow("BUTTON", "Button 1", WS_CHILD | WS_VISIBLE,
+		50, 50, 100, 30, hw, (HMENU)id_button1, GetModuleHandle(0), 0);
+
+	::CreateWindow("BUTTON", "Button 2", WS_CHILD | WS_VISIBLE,
+		50, 100, 100, 30, hw, (HMENU)id_button2, GetModuleHandle(0), 0);
 }
 
 void on_command(HWND hw, int id) 
 {
-	// TODO: show message box with text depending on which button was pressed
+	switch (id)
+	{
+	case id_button1:
+		::MessageBox(hw, "Button 1 clicked!", "Info", MB_OK | MB_ICONINFORMATION);
+		break;
+	case id_button2:
+		::MessageBox(hw, "Button 2 clicked!", "Info", MB_OK | MB_ICONINFORMATION);
+		break;
+	}
 }
 
 void on_destroy() 
@@ -43,7 +55,7 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));  // TODO: replace with cyan background
+	wc.hbrBackground = ::CreateSolidBrush(RGB(0, 255, 255));  // Cyan background
 	return ::RegisterClass(&wc);
 }
 
